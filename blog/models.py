159f9c900fd10b692c1f65f22037f77be01fa3d9
Symonnss <somborisymonss@gmail.com>
2018-11-19 13:49:15 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -18,3 +19,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
